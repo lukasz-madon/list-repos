bf081db0d7dc1f283f4a2187ed5a3cc0eed35f01
lukas madon <lukas@firestartr.co>
2014-07-05 14:37:08 +0100

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name listRepoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the listRepoApp
+ */
+angular.module('listRepoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.respos = [1,2,3];
+  });
---
+++ test/spec/controllers/about.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Controller: AboutCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('listRepoApp'));
+
+  var AboutCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    AboutCtrl = $controller('AboutCtrl', {
+      $scope: scope
+    });
+  }));
+
+//  it('nothing to test here', function () {
+//  });
+});
